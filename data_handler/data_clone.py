8bfd889912b3b610a63ec3743475b0ca81817d89
Huy Truong <huy.truong.210287@student.fulbright.edu.vn>
2023-12-05 00:17:22 +0700

    initial commit

---
+++ data_handler/data_clone.py
@@ -0,0 +1,27 @@
+import gdown
+import os
+from loguru import logger
+import zipfile
+
+def clone_data():
+    gg_drive_url = 'https://drive.google.com/uc?id=1jYxSQV5QVDTalFDbVSDBr1I4SPB-FTVb'
+    output = 'data.zip'
+
+    if (os.path.exists('./data')):
+        logger.info('Data is already found in ./data, no action is needed.')
+        return
+
+    if (not os.path.exists(output)):
+        logger.info('Downloading data from Google Drive...')
+        gdown.download(gg_drive_url, output, quiet=False)
+        logger.info('Data is downloaded.')
+    else:
+        logger.info('Data is already downloaded.')
+
+    logger.info('Extracting data...')
+    with zipfile.ZipFile(output, 'r') as zip_ref:
+        zip_ref.extractall()
+    logger.info('Data is extracted into ./data')
+
+
+clone_data()
